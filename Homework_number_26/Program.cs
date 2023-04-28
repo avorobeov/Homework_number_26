77ed78f0d23de4fe50c862066b9ffd65a1f89426
воробйов александр <avorobeov21@gmail.com>
2023-04-28 16:46:28 +0300

    Добавьте файлы проекта.

---
+++ Homework_number_26/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_number_26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Дана строка с текстом, используя метод строки String.Split() получить массив слов, которые разделены пробелом в тексте и вывести массив, каждое слово с новой строки.";
+            string[] words = text.Split(' ');
+
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
